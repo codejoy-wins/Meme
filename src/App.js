ee977cfead1be56860a0a796d4aafb97cf6d1983
Max <mjann@ucsc.edu>
2019-08-03 00:10:48 -0700

    proud

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
       topText: "",
       bottomText: "",
       randomImg: "https://clutchpoints.com/wp-content/uploads/2019/07/Should-you-trust-Jarvis-Landry-still-with-Odell-Beckham-Jr.-on-his-team.jpg", // initialize with something
+      color: ""
     }
     this.handleChange=this.handleChange.bind(this)
   }
@@ -25,12 +26,18 @@ class App extends React.Component {
         <form>
           <input name="topText" onChange={this.handleChange} placeholder="Top Text"/>
           <input name="bottomText" onChange={this.handleChange}  placeholder="Bottom Text"/>
-
+          <select onChange={this.handleChange} name="color">
+            <option value="">--Select an option--</option>
+            <option value="blue">Blue</option>
+            <option value="pink">Pink</option>
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+          </select>
         </form>
 
-        <h1>{this.state.topText}</h1>
+        <h1 style={{color: this.state.color}}>{this.state.topText}</h1>
         <img alt="fuck" className="widify" src={this.state.randomImg}></img>
-        <h1>{this.state.bottomText}</h1>
+        <h1 style={{color: this.state.color}}>{this.state.bottomText}</h1>
         <a href="https://maxjann.com">Jann Software</a>
       </div>
     )

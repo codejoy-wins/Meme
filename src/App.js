74457b40d5251de589ee2944dce40c2fb17aa252
Max <mjann@ucsc.edu>
2019-08-02 23:55:01 -0700

    meme generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      topText: "",
+      bottomText: "",
+      randomImg: "https://clutchpoints.com/wp-content/uploads/2019/07/Should-you-trust-Jarvis-Landry-still-with-Odell-Beckham-Jr.-on-his-team.jpg", // initialize with something
+    }
+    this.handleChange=this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form>
+          <input name="topText" onChange={this.handleChange} placeholder="Top Text"/>
+          <input name="bottomText" onChange={this.handleChange}  placeholder="Bottom Text"/>
+
+        </form>
+
+        <h1>{this.state.topText}</h1>
+        <img alt="fuck" className="widify" src={this.state.randomImg}></img>
+        <h1>{this.state.bottomText}</h1>
+
+      </div>
+    )
+  }
 }
 
 export default App;
